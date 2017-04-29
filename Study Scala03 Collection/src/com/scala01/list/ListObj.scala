a547acbc915e655f66ff7145575af9944984064f
followwwind <18771933975@163.com>
2017-04-29 15:40:59 +0800

    scala 03

---
+++ Study Scala03 Collection/src/com/scala01/list/ListObj.scala
@@ -0,0 +1,112 @@
+package com.scala01.list
+/**
+ * Scala 列表类似于数组，它们所有元素的类型都相同，但是它们也有所不同：
+		列表是不可变的，值一旦被定义了就不能改变，
+		其次列表 具有递归的结构（也就是链接表结构）
+		而数组不是。。列表的元素类型 T 可以写成 List[T]。
+	*/
+object ListObj {
+  //定义列表
+  def base(): Unit = {
+    // 字符串列表
+    val site: List[String] = List("Runoob", "Google", "Baidu")
+    println(site)
+    // 整型列表
+    val nums: List[Int] = List(1, 2, 3, 4)
+    // 空列表
+    val empty: List[Nothing] = List()
+    // 二维列表
+    val dim: List[List[Int]] =
+      List(
+        List(1, 0, 0),
+        List(0, 1, 0),
+        List(0, 0, 1))
+  }
+  //构造列表的两个基本单位是 Nil 和 ::  Nil 也可以表示为一个空列表。
+  def base2(): Unit = {
+    // 字符串列表
+    val site = "Runoob" :: ("Google" :: ("Baidu" :: Nil))
+    // 整型列表
+    val nums = 1 :: (2 :: (3 :: (4 :: Nil)))
+    // 空列表
+    val empty = Nil
+    // 二维列表
+    val dim = (1 :: (0 :: (0 :: Nil))) ::
+      (0 :: (1 :: (0 :: Nil))) ::
+      (0 :: (0 :: (1 :: Nil))) :: Nil
+  }
+  def method(): Unit = {
+    val site = "Runoob" :: ("Google" :: ("Baidu" :: Nil))
+    val nums = Nil
+    println("网站是 : " + site)
+    println("第一网站是 : " + site.head)
+    println("最后一个网站是 : " + site.tail)
+    println("查看列表 site 是否为空 : " + site.isEmpty)
+    println("查看 nums 是否为空 : " + nums.isEmpty)
+  }
+  def method2(): Unit = {
+    val site1 = "Runoob" :: ("Google" :: ("Baidu" :: Nil))
+    val site2 = "Facebook" :: ("Taobao" :: Nil)
+    // 使用 ::: 运算符
+    var fruit = site1 ::: site2
+    println("site1 ::: site2 : " + fruit)
+    // 使用 Set.:::() 方法
+    fruit = site1.:::(site2)
+    println("site1.:::(site2) : " + fruit)
+    // 使用 concat 方法
+    fruit = List.concat(site1, site2)
+    println("List.concat(site1, site2) : " + fruit)
+  }
+  def method3(): Unit = {
+    // List.fill() 方法来创建一个指定重复数量的元素列表：
+    val site = List.fill(3)("Runoob") // 重复 Runoob 3次
+    println("site : " + site)
+    val num = List.fill(10)(2) // 重复元素 2, 10 次
+    println("num : " + num)
+    /*List.tabulate() 方法是通过给定的函数来创建列表。
+          方法的第一个参数为元素的数量，可以是二维的，第二个参数为指定的函数，
+          我们通过指定的函数计算结果并返回值插入到列表中，起始值为 0*/
+    // 通过给定的函数创建 5 个元素
+    val squares = List.tabulate(6)(n => n * n)
+    println("一维 : " + squares)
+
+    // 创建二维列表
+    val mul = List.tabulate(4, 5)(_ * _)
+    println("多维 : " + mul)
+    val site1 = "Runoob" :: ("Google" :: ("Baidu" :: Nil))
+    println("site 反转前 : " + site1)
+    println("site 反转前 : " + site1.reverse)
+  }
+  //对集合的常用操作
+  def method4(): Unit = {
+    val left = List(1, 2, 3)
+    val right = List(4, 5, 6)
+    //以下操作等价
+    left ++ right // List(1,2,3,4,5,6)
+    left ++: right // List(1,2,3,4,5,6)
+    right.++:(left) // List(1,2,3,4,5,6)
+    right.:::(left) // List(1,2,3,4,5,6)
+    //以下操作等价
+    0 +: left //List(0,1,2,3)
+    var add=left.+:(0) //List(0,1,2,3)
+    println(add)
+    //以下操作等价
+    left :+ 4 //List(1,2,3,4)
+    var add2=left.:+(4) //List(1,2,3,4)
+    println(add2)
+    //以下操作等价
+    5 :: left //List(5,1,2,3)
+    println((5 :: left).sorted)
+    println((5 :: left).sorted)
+    left.::(0) //List(0,1,2,3)
+    println(left)  
+    right.foreach { i => print(i+" ") }
+  }
+  def main(args: Array[String]): Unit = {
+    //base()
+    //method()
+    //method2()
+    //method3()
+    method4()
+  }
+}
---
+++ Study Scala03 Collection/src/com/scala02/set/SetObj.scala
@@ -0,0 +1,39 @@
+package com.scala02.set
+
+import scala.collection.mutable.Set
+/*Scala Set(集合)是没有重复的对象集合，所有的元素都是唯一的。
+Scala 集合分为可变的和不可变的集合。
+默认情况下，Scala 使用的是不可变集合，如果你想使用可变集合，
+需要引用 scala.collection.mutable.Set 包。
+默认引用 scala.collection.immutable.Set*/
+object SetObj {
+  //不可变集合
+  def immutableSet(): Unit = {
+    val set = Set(1, 2, 3)
+    println(set.getClass.getName) // 
+    println(set.exists(_ % 2 == 0)) //true
+    println(set.drop(1)) //Set(2,3)
+    println(set)
+  }
+  //可变集合
+  /*注意： 虽然可变Set和不可变Set都有添加或删除元素的操作，
+ 		但是有一个非常大的差别。对不可变Set进行操作，会产生一个新的set，
+  	原来的set并没有改变，这与List一样。 而对可变Set进行操作，
+  	改变的是该Set本身，与ListBuffer类似。*/
+  def mutableSet(): Unit = {
+    import scala.collection.mutable.Set // 可以在任何地方引入 可变集合
+    val mutableSet = Set(1, 2, 3)
+    println(mutableSet.getClass.getName) // scala.collection.mutable.HashSet
+    mutableSet.add(4)
+    mutableSet.remove(1)
+    mutableSet += 5
+    mutableSet -= 2
+    println(mutableSet) // Set(5, 3, 4)
+    val another = mutableSet.toSet  //设置为不可变
+    println(another.getClass.getName) // scala.collection.immutable.Set
+  }
+  def main(args: Array[String]): Unit = {
+    immutableSet()
+    mutableSet()
+  }
+}
